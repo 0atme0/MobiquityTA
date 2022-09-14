6d7bee1050650bfc2a2390755a63462fa5870d10
Andrey I <iaa@bork.ru>
2022-09-14 16:44:27 +0200

    Initial Commit

---
+++ MobiquityTA/MobiquityTAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobiquityTAApp.swift
+//  MobiquityTA
+//
+//  Created by atme on 14/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct MobiquityTAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
