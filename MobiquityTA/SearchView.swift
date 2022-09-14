c9559182369fe51f0f0c296a7c0225a0167f6ff0
Andrey I <iaa@bork.ru>
2022-09-14 17:47:30 +0200

    create ui prototype

---
+++ MobiquityTA/SearchView.swift
@@ -0,0 +1,68 @@
+//
+//  SearchView.swift
+//  MobiquityTA
+//
+//  Created by atme on 14/09/2022.
+//
+
+import SwiftUI
+
+struct SearchResultItem: Identifiable, Codable {
+    var id = UUID()
+    var url = "http://farm1.static.flickr.com/578/23451156376_8983a8ebc7.jpg"
+}
+struct SearchView: View {
+    @State var searchText: String = ""
+    @State private var searchResults = [SearchResultItem(), SearchResultItem(), SearchResultItem(), SearchResultItem()]
+    @State private var searchHistory = ["First", "Second", "Third"]
+    @State private var selection: String = ""
+    private var grid = [GridItem(.flexible()), GridItem(.flexible())]
+    var body: some View {
+        VStack {
+            NavigationView {
+                ScrollView {
+                    LazyVGrid(columns: grid) {
+                        ForEach(searchResults, id: \.id) {
+                            AsyncImage(url: URL(string: $0.url)) { phase in
+                                switch phase {
+                                case .empty:
+                                    ProgressView()
+                                case .success(let image):
+                                    image.resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(maxWidth: 300, maxHeight: 100)
+                                case .failure:
+                                    Image(systemName: "photo")
+                                @unknown default:
+                                    EmptyView()
+                                }
+                            }
+                        }
+                    }
+                }
+                .searchable(text: $searchText, prompt: "Look for something") {
+                    ForEach(searchHistory, id: \.self) {
+                        Text($0.capitalized)
+                    }
+                }
+                .navigationTitle("Flickr Image Search")
+            }
+            .onSubmit(of: .search, runSearch)
+            Spacer()
+        }
+    }
+    
+    func runSearch() {
+        Task {
+            guard let url = URL(string: "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=28173ef65bf686a47baddfd9e3c147b3&text=\(searchText)&page=1&format=json&nojsoncallback=1&auth_token=72157720857177174-eab6545cff51b2af&api_sig=25786642e722e1c184408924b1b5617a") else { return }
+            let (data, _) = try await URLSession.shared.data(from: url)
+            searchResults = try JSONDecoder().decode([SearchResultItem].self, from: data)
+        }
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
